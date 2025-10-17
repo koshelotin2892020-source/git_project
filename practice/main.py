696cc1cbb1987e3675f3fe79c423779da233ad77
Alex <koshelotin2892020@gmail.com>
2025-10-17 20:28:58 +0300

    сделано 2 практика

--- practice/main.py
+++ practice/main.py
@@ -1,14 +1,35 @@
 def logger(func):
     def wrapper(*args, **kwargs):
-        print(f"Вызов функции {func.__name__} с аргументами {args} и {kwargs}")
+        if kwargs:
+            print(f"Вызов функции {func.__name__} с аргументами {args} и {kwargs}")
+        else:
+            print(f"Вызов функции {func.__name__} с аргументами {args}")
         result = func(*args, **kwargs)
         print(f"Функция {func.__name__} вернула {result}")
         return result
     return wrapper
 
 
-# def require_role(allowed_roles):
-#     def wrapper():
+def require_role(allowed_roles):
+    def decorator(func):
+        def wrapper(*args):
+            if args[0] in users:
+                if users[args[0]] in roles:
+                    return func(args[0])
+                else:
+                    print(f"Доступ запрещён пользователю {args[0]}")
+            else:
+                print(f"{args[0]} нету в users")
+        return wrapper
+    return decorator
+
+
+roles = ["admin"]
+
+users = {
+    "Alex": "admin",
+    "Ben": "manager"
+         }
 
 
 @logger
@@ -16,7 +37,7 @@ def add(a, b):
     try:
         return a + b
     except:
-        print("Надо ввести два числа")
+        print("ОШИБКА - Надо ввести два числа")
 
 
 @logger
@@ -25,8 +46,6 @@ def divide(a, b):
         return a / b
     except ZeroDivisionError:
         print("Ошибка деления на 0")
-    except:
-        print("Неизвестная ошибка")
 
 
 @logger
@@ -34,9 +53,21 @@ def greet(name):
     print(f"Hello, {name}")
 
 
-c = add(2, 10)
-# print(c)
+print("*"*20)
+add(2, 10)
+print("*"*20)
 divide(12, 0)
-d = divide(12, 2)
-print(d)
+print("*"*20)
+divide(12, 2)
+print("*"*20)
 greet("Alex")
+
+
+# @logger
+@require_role(roles)
+def delete_database(user):
+    print(f"База данных удалена пользователем {user}")
+
+
+print("*"*20)
+delete_database("Ben")
